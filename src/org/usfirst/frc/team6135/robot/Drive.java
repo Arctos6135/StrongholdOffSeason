532756cb887b89cba39006cc2d5a6ed224a66ee3
liphenste <liphenste@gmail.com>
2016-10-21 17:30:01 -0400

    Update Drive.java

--- src/org/usfirst/frc/team6135/robot/Drive.java
+++ src/org/usfirst/frc/team6135/robot/Drive.java
@@ -11,7 +11,7 @@ public class Drive {
 	private static final int minRate = 10;
 	private static final int sampleAverage = 14;
 	private static final double wheelRadius = 12; //Inches
-	private static final double wheelCircum = Math.PI * wheelRadius;
+	private static final double wheelCircum = 2 * Math.PI * wheelRadius;
 
 	//Drive constants
 	private static final double sensitivity = 0.3;
@@ -25,9 +25,12 @@ public class Drive {
 	private static final int angleRange = 10;
 	private static final double maxCompensate = 0.2;
 	private static final double compensateInc = 0.005;
-	private static final int lReverse = 1;
-	private static final int rReverse = 1;
+	private static final int lReverse = -1;
+	private static final int rReverse = -1;
 	private static final double motorCurvePow = 1.0; //The type of curve relating pwm to velocity
+	private static final boolean useSensitivityCalc = true;
+	private static final boolean useAccelerateCalc = true;
+	private static final boolean useCompensate = true;
 
 	//Object Declarations
 	private Joystick driveStick = null;
@@ -35,7 +38,7 @@ public class Drive {
 	private Victor rightDrive = null;
 	private Encoder leftEnc = null;
 	private Encoder rightEnc = null;
-
+	
 	//Variables Used
 	private double xInput;
 	private double yInput;
@@ -46,6 +49,8 @@ public class Drive {
 	private double rCompensate = 0.0;
 	private double lSpeed = 0.0;
 	private double rSpeed = 0.0;
+	private double lPrev = 0.0;
+	private double rPrev = 0.0;
 
 	public Drive(Joystick j, int rDrive, int lDrive, int lEnc1, int lEnc2, int rEnc1, int rEnc2) {
 		driveStick = j;
@@ -57,40 +62,27 @@ public class Drive {
 		rightEnc.setMinRate(minRate);
 		leftEnc.setSamplesToAverage(sampleAverage);
 		rightEnc.setSamplesToAverage(sampleAverage);
+		leftEnc.setDistancePerPulse(wheelCircum);
+		rightEnc.setDistancePerPulse(wheelCircum);
 		leftDrive.set(0);
 		rightDrive.set(0);
 	}
-	private double sensitivityCalc(double input) {
-		return sensitivity * input + (1 - sensitivity) * Math.pow(input, inputPow);
-	}
-	private double sensitivity2Calc(double input) {
-		if (input >= 0.0) {
-	        return (input * input);
-        }
-		else {
-    	    return -(input * input);
-    	}
-	}
 	private double angle(double x, double y) {
-		return Math.toDegrees(Math.atan2(x, y));
+	    return Math.toDegrees(Math.atan2(x, y));
 	}
-	private double accCalc(double input) {
-		if(input > accBound && accLoopCount < accLoop) {
-	    		return accBound + (input - accBound) * (accLoopCount++ / (double) accLoop);
-	    }
-		else if(input < -accBound && accLoopCount < accLoop) {
-	    		return -accBound + (input + accBound) * (accLoopCount++ / (double) accLoop);
-	    }
-	    else if(Math.abs(input) <= 0.7) {
-	    	accLoopCount = 0;
-	    }
-		return input;
-	}
-	private void encReset() {
+	public void encReset() {
 		leftEnc.reset();
 		rightEnc.reset();
 		lCompensate = 0.0;
 		rCompensate = 0.0;
+	}
+	public double getleftDis()
+	{
+		return leftEnc.getDistance();
+	}
+	public double getRightDis()
+	{
+		return rightEnc.getDistance();
 	}
 	private void compensate() {
 		if(leftEnc.get() > Math.pow((lSpeed / rSpeed), motorCurvePow) * (rightEnc.get() + 1)) {
@@ -126,18 +118,9 @@ public class Drive {
         	}
     	}
 	}
-	public void getValues() {
-		xFinal = (xInput = xReverse * driveStick.getRawAxis(xAxis));
-		yFinal = (yInput = yReverse * driveStick.getRawAxis(yAxis));
-		xFinal = sensitivity2Calc(xFinal);
-		yFinal = sensitivity2Calc(yFinal);
-
-		xFinal = accCalc(xFinal);
-		yFinal = accCalc(yFinal);
-	}
 	public void getValues(double x, double y, boolean sensitivity, boolean acc) {
-		xFinal = (xInput = xReverse * x);
-		yFinal = (yInput = yReverse * y);
+		xFinal = x;
+		yFinal = y;
 		
 		if(sensitivity) {
 			xFinal = sensitivity2Calc(xFinal);
@@ -149,23 +132,73 @@ public class Drive {
 			yFinal = accCalc(yFinal);
 		}
 	}
-	
-	public void setMotors() {
-		leftDrive.set(lReverse * (lSpeed + lCompensate));
-		rightDrive.set(rReverse * (rSpeed + rCompensate));
-	}
 	public void setMotors(double l, double r) {
-		leftDrive.set(lReverse * (l + lCompensate));
-		rightDrive.set(rReverse * (r + rCompensate));
+		leftDrive.set(lReverse * Math.signum(lPrev = l) * (Math.abs(l) + lCompensate));
+		rightDrive.set(rReverse * Math.signum(rPrev = r) * (Math.max(Math.abs(r) + rCompensate, 0)));
+	}
+	
+	//Teleop Driving methods
+	private double accCalc(double input) {
+		if(input > accBound && accLoopCount < accLoop) {
+	    		return accBound + (input - accBound) * (accLoopCount++ / (double) accLoop);
+	    }
+		else if(input < -accBound && accLoopCount < accLoop) {
+	    		return -accBound + (input + accBound) * (accLoopCount++ / (double) accLoop);
+	    }
+	    else if(Math.abs(input) <= 0.7) {
+	    	accLoopCount = 0;
+	    }
+		return input;
+	}
+	private double sensitivityCalc(double input) {
+		return sensitivity * input + (1 - sensitivity) * Math.pow(input, inputPow);
+	}
+	private double sensitivity2Calc(double input) {
+		if (input >= 0.0) {
+	        return (input * input);
+        }
+		else {
+    	    return -(input * input);
+    	}
+	}
+	private void exactDrive() {
+		if(Math.abs(yInput) > Math.abs(xInput)) {
+			xInput = 0;
+		}
+		else {
+			yInput = 0;
+		}
 	}
 	public void teleopDrive() {
-		getValues();
+		xInput = xReverse * driveStick.getRawAxis(xAxis);
+		yInput = yReverse * driveStick.getRawAxis(yAxis);
+		if(Math.abs(xInput) < 0.1 && Math.abs(yInput) < 0.1 ) {
+			setMotors(0, 0);
+		}
+		if(driveStick.getRawButton(Constants.exactDriveButton)) {
+			exactDrive();
+		}
+		
+		getValues(xInput, yInput, useSensitivityCalc, useAccelerateCalc);
+		
 		getSpeeds();
-		if(Math.abs(angle(lReverse * leftDrive.get() - lCompensate, rReverse * rightDrive.get() - rCompensate) - angle(lSpeed, rSpeed)) > angleRange) {
+		
+		if(Math.abs(angle(lPrev, rPrev) - angle(lSpeed, rSpeed)) > angleRange) {
 			encReset();
 		}
+		if(useCompensate) {
+			compensate();
+		}
+		setMotors(lSpeed, rSpeed);
+	}
+	public void autoDrive(double speed)
+	{
 		compensate();
-		setMotors();
+		setMotors(speed,speed);
+	}
+	public void autoDrive(double l,double r)
+	{
+		setMotors(l,r);
 	}
 	public String toString() {
 		return "xInput: " + xInput + "\n" +
@@ -175,6 +208,8 @@ public class Drive {
 				"lCompensate: " + lCompensate + "\n" +
 				"rCompensate: " + rCompensate + "\n" +
 				"lSpeed: " + lSpeed + "\n" +
-				"rSpeed: " + rSpeed + "\n";
+				"rSpeed: " + rSpeed + "\n"+
+				"lRate: "+leftEnc.getRate()+"\n"+
+				"rRate+ "+rightEnc.getRate()+"\n";
 	}
 }
